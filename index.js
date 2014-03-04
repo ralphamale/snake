d0c97f795b0592134101404a88a7d997cdd5b784
Ralph Nguyen <ralph.nguyen@gmail.com>
2014-03-04 12:15:20 -0500

    Major refactoring.

--- index.js
+++
@@ -1,8 +0,0 @@
-// require("./lib/util.js");
-// require("./lib/moving_object.js");
-// require("./lib/asteroid.js");
-// require("./lib/bullet.js");
-// require("./lib/ship.js");
-// require("./lib/game.js");
-
-module.exports = global.Snake;
